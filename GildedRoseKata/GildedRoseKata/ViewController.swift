e61c6f53ddc7ddb8653ac5a9a1a0cfe0f1c729a1
Shaher Kassam <shaher.kassam@gmail.com>
2019-03-08 15:36:17 +0100

    Create Xcode Project Remove Package

---
+++ GildedRoseKata/GildedRoseKata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GildedRoseKata
+//
+//  Created by Shaher Kassam on 08/03/2019.
+//  Copyright © 2019 Shaher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
