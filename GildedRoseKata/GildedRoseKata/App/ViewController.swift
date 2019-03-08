ca9c17522006026b15a1ddcd815af553950272f2
Shaher Kassam <shaher.kassam@gmail.com>
2019-03-08 15:51:45 +0100

    Test Text working

---
+++ GildedRoseKata/GildedRoseKata/App/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  GildedRoseKata
+//
+//  Created by Shaher Kassam on 08/03/2019.
+//  Copyright © 2019 Shaher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        main()
+        
+    }
+
+    //TextTest
+    func main() {
+        
+        let items = [
+            Item(name: "+5 Dexterity Vest", sellIn: 10, quality: 20),
+            Item(name: "Aged Brie", sellIn: 2, quality: 0),
+            Item(name: "Elixir of the Mongoose", sellIn: 5, quality: 7),
+            Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80),
+            Item(name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80),
+            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 15, quality: 20),
+            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 49),
+            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 49),
+            // this conjured item does not work properly yet
+            Item(name: "Conjured Mana Cake", sellIn: 3, quality: 6)]
+        
+        let app = GildedRose(items: items);
+        
+        var days = 2;
+        if (CommandLine.argc > 1) {
+            days = Int(CommandLine.arguments[1])! + 1
+        }
+        
+        
+        for i in 0..<days {
+            print("-------- day \(i) --------");
+            print("name, sellIn, quality");
+            for item in items {
+                print(item);
+            }
+            print("");
+            app.updateQuality();
+        }
+        
+    }
+
+}
+
